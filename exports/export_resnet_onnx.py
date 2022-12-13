56582126e6dbc9f09ae308c1985f49faf674816e
log <rizhiding@163.com>
2022-12-13 16:02:19 +0800

    change

--- exports/export_resnet_onnx.py
+++ exports/export_resnet_onnx.py
@@ -1,10 +1,16 @@
 import yaml
 from main import build_test_data_loader
 import dataset
+from PIL import Image
 from models.backbone_resnet50 import Resnet50
 import torch
+import onnxruntime
+from dataloader.classify_dataset import ClassifyDataset
+from torchvision import transforms
 from postprocessing.caculate import predict_anomaly_score
 from postprocessing.plot import generate_image
+import glob
+from scipy.special import softmax
 
 
 def convert_to_resnet_onnx(model_pth, output_path):
@@ -48,5 +54,37 @@ def convert_to_resnet_onnx(model_pth, output_path):
                                     } if dynamic else None)
 
 
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+
+def check_export(onnx_path_demo, image_path):
+    test_transformer = transforms.Compose([
+        transforms.Resize((256, 256), interpolation=3),
+        # transforms.CenterCrop(size=opt.height),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ])
+
+    session = onnxruntime.InferenceSession(onnx_path_demo)
+
+    images = glob.glob(image_path)
+    for img_ in images:
+        image = Image.open(img_)
+        image = test_transformer(image)
+        image = image.unsqueeze(0)
+        image = to_numpy(image)
+
+        input_name = session.get_inputs()[0].name
+        outputs = session.run(None, {input_name: image})[0]
+        outputs = softmax(outputs, axis=1)
+
+        # print(outputs[0].shape, outputs[1].shape, outputs[2].shape)
+        print(img_)
+        print(outputs)
+
+
 if __name__ == '__main__':
-    convert_to_resnet_onnx("../_experiment_checkpoints/exp95_resnet50_2022-12-13-10-57/epoch_44_model.pth", "../_exports/resnet50.onnx")
+    # convert_to_resnet_onnx("../_experiment_checkpoints/exp95_resnet50_2022-12-13-10-57/epoch_44_model.pth", "../_exports/resnet50.onnx")
+
+    check_export("../_exports/resnet50.onnx", "../_exports/*.jpg")
